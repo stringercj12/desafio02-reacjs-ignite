72f3c990fa6f47ede0fd1ef12f867d1b863c44ba
Jefferson Ferreira <jefferson14489@gmail.com>
2021-12-02 11:03:51 -0300

    Resolvendo desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (genreId: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SidebarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
